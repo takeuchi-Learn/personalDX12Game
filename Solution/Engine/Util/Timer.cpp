fbeb4e6c61e4647d1946d3e05a82992236d67d9b
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2022-11-01 13:37:03 +0900

    Timerクラスのリファクタリング

--- Solution/Engine/Util/Timer.cpp
+++ Solution/Engine/Util/Timer.cpp
@@ -15,10 +15,3 @@ Timer::Timer() :
 	startTimeDir(steady_clock::now())
 {
 }
-
-Timer::timeType Timer::getNowTime()
-{
-	return duration_cast<timeUnit>(steady_clock::now() - startTimeDir).count();
-}
-
-void Timer::reset() { startTimeDir = steady_clock::now(); }
--- Solution/Engine/Util/Timer.h
+++ Solution/Engine/Util/Timer.h
@@ -7,7 +7,7 @@
 
 #include <chrono>
 
-/// @brief 時間取得クラス
+ /// @brief 時間取得クラス
 class Timer
 {
 private:
@@ -33,11 +33,13 @@ public:
 	/// @return 一拍の時間
 	inline static timeType getOneBeatTime(const float bpm) { return timeType(std::chrono::duration_cast<timeUnit>(std::chrono::seconds(60ll)).count() / bpm); };
 
-	/// @brief reset()最後に実行した時間が起点
-	/// @brief 実行していなければクラス生成時の時間が起点
-	/// @return 現在までの時間
-	timeType getNowTime();
+	/// @brief 起点時間から現在までの時間を取得
+	/// @return 起点時間から現在までの時間
+	inline timeType getNowTime()
+	{
+		return duration_cast<timeUnit>(steady_clock::now() - startTimeDir).count();
+	}
 
-	/// @brief 現在を0とする
-	void reset();
+	/// @brief 現在を起点時間とする(現在を0とする)
+	inline void reset() { startTimeDir = steady_clock::now(); }
 };
