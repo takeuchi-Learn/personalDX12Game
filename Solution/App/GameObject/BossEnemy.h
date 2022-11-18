5cd79f7ada8e064db92386782f8fa92e0a29a25a
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2022-11-18 16:05:44 +0900

    BossEnemyから不要なものを削除

--- Solution/App/GameObject/BossEnemy.h
+++ Solution/App/GameObject/BossEnemy.h
@@ -3,19 +3,7 @@
 class BossEnemy :
 	public BaseEnemy
 {
-private:
-	uint16_t hp;
-
 public:
-	BossEnemy(Camera* camera,
-			  ObjModel* model,
-			  const DirectX::XMFLOAT3& pos = { 0,0,0 },
-			  uint16_t hp = 3u);
-
-	/// @brief ダメージを与える
-	/// @param damegeNum 与えるダメージ数
-	/// @param killFlag hpが0になったらkillするかどうか(trueでkillする)
-	/// @return 倒したかどうか(倒したらtrue)
-	bool damage(uint16_t damegeNum, bool killFlag = true);
+	using BaseEnemy::BaseEnemy;
 };
 
