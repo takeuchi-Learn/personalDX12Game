ad4f724601a47af21fa7c613b5a33e4ff65e5644
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2022-11-18 16:49:51 +0900

    ボスの行動を遷移するようにした。自機への接近と離脱を繰り返している。

--- Solution/App/GameObject/BossEnemy.cpp
+++ Solution/App/GameObject/BossEnemy.cpp
@@ -2,26 +2,19 @@
 
 using namespace DirectX;
 
-void BossEnemy::phase_approach(GameObj* targetObj)
+DirectX::XMVECTOR BossEnemy::calcVelVec(GameObj* targetObj)
 {
 	XMVECTOR velVec = XMLoadFloat3(&targetObj->getPos()) - XMLoadFloat3(&getPos());
 
 	// Y方向には移動しない
-	velVec = XMVectorSetY(velVec, 0.f);
+	return XMVectorSetY(velVec, 0.f);
+}
 
-	// 一定距離より近ければ移動しない
-	if (XMVectorGetX(XMVector3Length(velVec)) < getScaleF3().x)
-	{
-		return;
-	}
-
-	// 大きさを反映
-	constexpr float speed = 2.f;
-	velVec = XMVector3Normalize(velVec) * speed;
-
-	// XMFLOAT3に変換
+void BossEnemy::moveAndRota(float moveSpeed, const DirectX::XMVECTOR& velVec)
+{
+	// 大きさを反映した速度をXMFLOAT3に変換
 	XMFLOAT3 vel{};
-	XMStoreFloat3(&vel, velVec);
+	XMStoreFloat3(&vel, XMVector3Normalize(velVec) * moveSpeed);
 
 	// 移動
 	move(vel);
@@ -29,4 +22,34 @@ void BossEnemy::phase_approach(GameObj* targetObj)
 	// 速度に合わせて回転
 	const XMFLOAT2 rotaDeg = GameObj::calcRotationSyncVelDeg(vel);
 	setRotation(XMFLOAT3(rotaDeg.x, rotaDeg.y, 0.f));
+}
+
+void BossEnemy::phase_approach(GameObj* targetObj)
+{
+	XMVECTOR velVec = calcVelVec(targetObj);
+
+	// 一定距離より近ければ遠ざかる
+	if (XMVectorGetX(XMVector3Length(velVec)) < getScaleF3().x)
+	{
+		setPhase(std::bind(&BossEnemy::phase_leave, this, targetObj));
+		return;
+	}
+
+	// 大きさを反映
+	moveAndRota(moveSpeed, velVec);
+}
+
+void BossEnemy::phase_leave(GameObj* targetObj)
+{
+	XMVECTOR velVec = calcVelVec(targetObj);
+
+	// 一定距離より遠ければ近づく
+	if (XMVectorGetX(XMVector3Length(velVec)) > getScaleF3().x * 5.f)
+	{
+		setPhase(std::bind(&BossEnemy::phase_approach, this, targetObj));
+		return;
+	}
+
+	// 大きさを反映
+	moveAndRota(moveSpeed, -velVec);
 }
--- Solution/App/GameObject/BossEnemy.h
+++ Solution/App/GameObject/BossEnemy.h
@@ -3,9 +3,18 @@
 class BossEnemy :
 	public BaseEnemy
 {
+
+	float moveSpeed = 2.f;
+
+	DirectX::XMVECTOR calcVelVec(GameObj* targetObj);
+
+	void moveAndRota(float moveSpeed, const DirectX::XMVECTOR& velVec);
+
 public:
 	using BaseEnemy::BaseEnemy;
 
 	void phase_approach(GameObj* targetObj);
 	inline void changePhase_approach(GameObj* targetObj) { setPhase(std::bind(&BossEnemy::phase_approach, this, targetObj)); }
+	
+	void phase_leave(GameObj* targetObj);
 };
